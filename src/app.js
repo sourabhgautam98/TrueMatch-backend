37357b9bcd2ddd1bc408e31a114dd8166badf923
sourabh gautam <gautamsourabh98.com>
2026-03-01 23:35:25 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -10,10 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
-  ],
+  origin: true,
   credentials: true,
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
